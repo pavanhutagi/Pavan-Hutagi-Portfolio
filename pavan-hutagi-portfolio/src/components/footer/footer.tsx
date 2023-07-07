c44f8cc8228226babe7115cfbcec1f50d27f446a
Pavan Hutagi <pavanhutagi@gmail.com>
2023-07-07 17:36:22 +0530

    Adding footer

--- pavan-hutagi-portfolio/src/components/footer/footer.tsx
+++ pavan-hutagi-portfolio/src/components/footer/footer.tsx
@@ -1,7 +1,13 @@
+import { Container } from "react-bootstrap";
+
 export default function Footer() {
   return (
     <>
-      <div className="w-full h-32 bg-gray-500"></div>
+      <div className="w-full h-32 bg-gray-500">
+        <Container className="h-full flex flex-row  text-white text-xl align-middle">
+          <div className="m-auto flex justify-center"> Pavan Hutagi </div>
+        </Container>
+      </div>
     </>
   );
 }
--- pavan-hutagi-portfolio/src/components/nav-bar/nav-bar.tsx
+++ pavan-hutagi-portfolio/src/components/nav-bar/nav-bar.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <>
       <div className="w-full h-20 bg-gray-500 fixed">
-        <Container className="h-full flex flex-row  text-white text-xl inline-block align-middle">
+        <Container className="h-full flex flex-row  text-white text-xl align-middle">
           <div className="m-auto flex-1"> Pavan Hutagi </div>
           <div className="m-auto flex justify-center w-40"> Home </div>
           <div className="m-auto flex justify-center w-40"> About </div>
