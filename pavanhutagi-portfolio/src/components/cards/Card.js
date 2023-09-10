26a3c3ea5f4b820c51fa5684422b9c9a519446c3
Pavan Hutagi <pavanhutagi@gmail.com>
2023-09-10 20:10:56 +0530

    Adding 3D card

---
+++ pavanhutagi-portfolio/src/components/cards/Card.js
@@ -0,0 +1,21 @@
+"use client";
+
+import "./Card.css";
+import "./CardAnimation";
+
+export default function Card() {
+  return (
+    <>
+      <div class="cover atvImg">
+        <div
+          class="atvImg-layer"
+          data-img="http://robindelaporte.fr/codepen/visa-bg-2.jpg"
+        ></div>
+        <div
+          class="atvImg-layer"
+          // data-img="http://robindelaporte.fr/codepen/visa.png"
+        ></div>
+      </div>
+    </>
+  );
+}
---
+++ pavanhutagi-portfolio/src/components/particles/LineParticles.js
@@ -0,0 +1,25 @@
+import "./LineParticles.css";
+
+import particlesConfigJson from "@/configs/particles-config.json";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { Engine } from "tsparticles-engine";
+
+export default function LineParticles() {
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  let particlesConfig = JSON.parse(JSON.stringify(particlesConfigJson));
+
+  return (
+    <>
+      <Particles
+        id="tsparticles1"
+        init={particlesInit}
+        options={particlesConfig}
+        className="particles"
+      />
+    </>
+  );
+}
